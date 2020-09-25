c8c07f469018c48a2cfb452145477fc913fb415e
sugijo <sugi.jotaro@gmail.com>
2020-09-25 20:04:58 +0900

    sound effect

--- soundeffect/ViewController.swift
+++ soundeffect/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
+    let soundName = ["","nyu1","nyu2","nyu3","puyon1","eye-shine1","shakin1","shine1","sceneswitch1","tirin1","shock1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton){
+        let soundURL = Bundle.main.url(forResource: soundName[sender.tag], withExtension: "mp3")
+        do {
+            // 効果音を鳴らす
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+            player?.play()
+        } catch {
+            print("error...")
+        }
+    }
+    
 }
 
